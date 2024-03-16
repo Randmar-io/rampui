d85ed9fac10a26ce1026a0d5279c921300ead1d1
Abdullah Raja <68147057+wryhook@users.noreply.github.com>
2024-03-16 16:02:41 -0700

    Custom style + bug fixes

--- src/Image/Image.stories.tsx
+++ src/Image/Image.stories.tsx
@@ -17,14 +17,28 @@ const meta: Meta<typeof Image> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const WithLink: Story = {
+  render: () => (
+    <a href="https://www.pexels.com/photo/19845824" style={{ display: "inline-block" }}>
+      <Image
+        src="https://images.pexels.com/photos/19845824/pexels-photo-19845824/free-photo-of-a-grassy-hill-with-a-beach-and-ocean-in-the-background.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt="A beautiful beach"
+        size="xl"
+      />
+    </a>
+  ),
+  args: {
+    size: 'md',
+  },
+};
+
+export const Zoomable: Story = {
   render: () => (
     <Image
       src="https://images.pexels.com/photos/19845824/pexels-photo-19845824/free-photo-of-a-grassy-hill-with-a-beach-and-ocean-in-the-background.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       alt="A beautiful beach"
-      zoomable
-      aspectRatio='auto'
       size="xl"
+      zoomable
     />
   ),
   args: {
