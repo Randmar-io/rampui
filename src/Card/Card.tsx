ce2f297bd8f98bfc162e709c3350467a5e323740
Abdullah Raja <68147057+wryhook@users.noreply.github.com>
2024-05-15 14:48:02 -0700

    Add tags to card

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -1,10 +1,12 @@
 import { Stack } from "@mui/material";
 import { Icon as PhosphorIcon } from "@phosphor-icons/react";
 import React from "react";
+import { Chip, ChipProps } from "../Chip";
 import { Paper } from "../Paper";
 
 export interface CardProps {
-  title?: string;
+  title?: string | React.ReactNode;
+  tags?: ChipProps[];
   icon?: PhosphorIcon;
   children?: React.ReactNode;
   actions?: React.ReactNode[];
@@ -12,31 +14,34 @@ export interface CardProps {
   headerTextColor?: string;
 }
 
-export function Card({ title, icon: Icon, children, actions, headerColor, headerTextColor }: CardProps) {
+export function Card({ title, tags, icon: Icon, children, actions, headerColor, headerTextColor }: CardProps) {
   return (
     <Paper>
-      <Stack spacing={2}>
-        <Stack
-          direction="row"
-          alignItems="center"
-          spacing={1}
-          style={{
-            margin: '-16px',
-            padding: '16px',
-            backgroundColor: headerColor,
-            color: headerTextColor,
-            borderRadius: '12px 12px 0 0'
-          }}
-        >
-          {Icon && <Icon size={20} />}
-          <p style={{ fontSize: 16, fontWeight: 600 }}>{title}</p>
-        </Stack>
-        <div>
-          {children}
+      {tags &&
+        <div style={{ marginBottom: 8, display: 'flex', alignItems: 'center', gap: 4, flexWrap: 'wrap' }}>
+          {tags.map((tag, index) => <Chip key={index} {...tag} />)}
         </div>
-        <Stack direction="row" justifyContent="flex-end" spacing={0.5}>
-          {actions}
-        </Stack>
+      }
+      <Stack
+        direction="row"
+        alignItems="center"
+        spacing={1}
+        style={{
+          margin: '-16px',
+          padding: '16px',
+          backgroundColor: headerColor,
+          color: headerTextColor,
+          borderRadius: '12px 12px 0 0'
+        }}
+      >
+        {Icon && <Icon size={20} />}
+        <p style={{ fontSize: 16, fontWeight: 600 }}>{title}</p>
+      </Stack>
+      <div style={{ marginTop: 12 }}>
+        {children}
+      </div>
+      <Stack direction="row" justifyContent="flex-end" spacing={0.5}>
+        {actions}
       </Stack>
     </Paper>
   );
