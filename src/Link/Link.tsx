d54e1afeea506cf734c1dfa5ac0ae1cfb42b2872
Abdullah Raja <68147057+wryhook@users.noreply.github.com>
2024-05-15 04:28:50 -0700

    Add Chip and Link components

---
+++ src/Link/Link.tsx
@@ -0,0 +1,42 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { ArrowUpRight } from "@phosphor-icons/react/dist/ssr";
+import React, { forwardRef } from "react";
+import { colors } from "../colors/util";
+
+interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  color?: "default" | "reseller" | "manufacturer" | "shopify" | "success" | "error";
+  external?: boolean;
+}
+
+const StyledLink = styled('a')<LinkProps>((props) => {
+  const color = props.color ? colors[props.color] : props.theme.color;
+
+  return css`
+    text-decoration: none;
+    color: ${color[700]};
+    border-bottom: 1px solid ${color[600]};
+    display: flex;
+    align-items: center;
+    gap: var(--r-spacing-5);
+    width: max-content;
+    margin-bottom: var(--r-spacing-80);
+
+    &:hover {
+      border-color: transparent;
+    }
+  `
+})
+
+export const Link = forwardRef<HTMLAnchorElement, LinkProps>(({ external, children, ...props }, ref) => {
+  return (
+    <StyledLink
+      ref={ref}
+      {...props}
+      target={external ? "_blank" : props.target}
+    >
+      <span>{children}</span>
+      {external && <ArrowUpRight size={16} />}
+    </StyledLink>
+  );
+});
---
+++ src/colors/util.ts
@@ -0,0 +1,16 @@
+import blue from "./blue";
+import green from "./green";
+import grey from "./grey";
+import lightGreen from "./lightGreen";
+import purple from "./purple";
+import red from "./red";
+
+export const colors = {
+  "default": grey,
+  "central": red,
+  "reseller": blue,
+  "manufacturer": purple,
+  "shopify": lightGreen,
+  "success": green,
+  "error": red,
+}
