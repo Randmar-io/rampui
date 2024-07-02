7fe4d36916fe27279082fedd5ccc05dd3a8614ee
Abdullah <a@randmar.io>
2024-07-02 14:58:58 +0400

    Add loading state

--- src/AppShell/AppShell.stories.tsx
+++ src/AppShell/AppShell.stories.tsx
@@ -43,6 +43,7 @@ export const AppShellWithItems = () => {
 
   const footer = (
     <NavFooter
+      loading={true}
       profileImageUrl="https://api.randmar.io/v4/Partner/2/Account/Logo?width=64&height=64"
       profileName="Brother"
       profileEmail="admin@brother.ca"
